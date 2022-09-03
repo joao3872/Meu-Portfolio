ebc6de40b6a4762aca2b13558c027544df60bbce
João Matheus <joaomatheus11@gmail.com>
2022-09-02 22:22:30 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -25,52 +25,28 @@ function fecharMenu() {
 }
 
 
+// Escutador de eventos para ativar a Arrow Function, quando qualquer um dos links do menu for clicado.
+
 linksDoMenu.addEventListener('click', () => {
     fecharMenu()
 })
 
-/*function abrirMenu() {
-    var menu = document.querySelector('nav.menu')
-    var iconMenu = document.querySelector('span.icon-menu')
-    var iconClose = document.querySelector('span.close-menu')
-
-    menu.style.display = 'block'
-    iconMenu.style.display = 'none'
-    iconClose.style.display = 'block'
-}*/
-
-
-/*function fecharMenu() {
-    var menu = document.querySelector('nav.menu')
-    var iconMenu = document.querySelector('span.icon-menu')
-    var iconClose = document.querySelector('span.close-menu')
-
-    menu.style.display = 'none'
-    iconMenu.style.display = 'block'
-    iconClose.style.display = 'none'
-}*/
-
-
 
 function abrirContatos() {
-    /*var modal = document.querySelector('section.fundo-modal')*/
     modal.style.display = 'block'
 }
 
 function fecharContatos() {
-    /*var modal = document.querySelector('section.fundo-modal')*/
     modal.style.display = 'none'
 }
 
 
 
 function modalInformacoes() {
-    /*var informacoes = document.querySelector('.modal_informacoes')*/
     informacoes.style.display = 'block'
 }
 
 
 function fecharInformacoes() {
-    /*var informacoes = document.querySelector('.modal_informacoes')*/
     informacoes.style.display = 'none'
 }
