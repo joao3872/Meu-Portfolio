7fcc4e0f9c61c8adf28528e64c0dea7ed2a4026e
João Matheus <joaomatheus11@gmail.com>
2024-11-23 12:10:55 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -9,6 +9,10 @@ let modal = document.querySelector('.fundo-modal')
 
 let informacoes = document.querySelector('.modal_informacoes')
 
+let btn = document.querySelectorAll('.btn')
+
+let icons = document.querySelectorAll('.icons')
+
 
 function abrirMenu() {
     menu.style.display = 'block'
@@ -45,3 +49,38 @@ function modalInformacoes() {
 function fecharInformacoes() {
     informacoes.style.display = 'none'
 }
+
+
+
+// Função criada para substituir o ícone, quando o cursor do mouse, está dentro do botão.
+
+function hoverOn() {
+    for (let c = 0; c < btn.length; c++) {
+        btn[c].addEventListener('mouseenter', () => {
+            if (btn[c] == btn[0]) {
+                icons[0].setAttribute('src', 'imagens/github-brands-solid2.svg')
+            } else if (btn[c] == btn[1]) {
+                icons[1].setAttribute('src', 'imagens/linkedin-brands-solid2.svg')
+            }
+        })
+    }
+}
+
+hoverOn()
+
+
+// Função para desfazer a substituição dos ícones, quando o cursor do mouse estiver fora do botão.
+
+function hoverOff() {
+    for (let c = 0; c < btn.length; c++) {
+        btn[c].addEventListener('mouseleave', () => {
+            if (btn[c] == btn[0]) {
+                icons[0].setAttribute('src', 'imagens/github-brands-solid.svg')
+            } else if (btn[c] == btn[1]) {
+                icons[1].setAttribute('src', 'imagens/linkedin-brands-solid.svg')
+            }
+        })
+    }
+}
+
+hoverOff()
